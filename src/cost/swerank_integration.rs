aca0402f2b5be288e2b62950455c98edd7f11e1a
Nick Russo <nijaru7@gmail.com>
2025-07-20 15:55:27 -0700

    Clean up unused imports
    - Remove unused toml and toml_config imports from CLI module
    - Fix debug macro import in swerank_integration module
    - Reduce compile warnings for better code quality

--- src/cost/swerank_integration.rs
+++ src/cost/swerank_integration.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use std::path::PathBuf;
-use tracing::{info, warn};
+use tracing::{debug, info, warn};
 
 use candle_core::Device;
 use candle_transformers::models::bert::BertModel;
